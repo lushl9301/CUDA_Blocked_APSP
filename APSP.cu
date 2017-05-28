2c2f98d0772ff4d7c3f7edb158d46334134c4940
lushl9301 <lushl9301@gmail.com>
2017-05-29 00:52:28 +0800

    <add> half precision float data type; turns to be slower

--- APSP.cu
+++ APSP.cu
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <cuda_fp16.h>
+#include <assert.h>
+#include "fp16_conversion.h"
 
 #include <cuda_runtime.h>
 #include <cuda_profiler_api.h>
@@ -24,9 +30,9 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 
 
 
-__global__ void floyd_kernel_1(int k, int *devMat, int N)
+__global__ void floyd_kernel_1(int k, half *devMat, int N)
 {
-	__shared__ int d_d[BLKSZ_1][BLKSZ_1];
+	__shared__ half d_d[BLKSZ_1][BLKSZ_1];
 	int base = k * BLKSZ_1;
 	int i = threadIdx.y;
 	int j = threadIdx.x;
@@ -36,20 +42,20 @@ __global__ void floyd_kernel_1(int k, int *devMat, int N)
 	d_d[i][j] = devMat[base];
 	__syncthreads();
 
-	int newD;
+	half newD;
 	for (int t = 0; t < BLKSZ_1; t++) {
-		newD = d_d[i][t] + d_d[t][j];
-		if (newD < d_d[i][j])
+		newD = __hadd(d_d[i][t], d_d[t][j]);
+		if (__hlt(newD, d_d[i][j]))
 			d_d[i][j] = newD;
 	}
 	devMat[base] = d_d[i][j];
 }
 
-__global__ void floyd_kernel_2(int k, int *devMat, int N)
+__global__ void floyd_kernel_2(int k, half *devMat, int N)
 {
 	if (blockIdx.x == k) return;
 
-	__shared__ int d_d[BLKSZ_2][BLKSZ_2], d_c[BLKSZ_2][BLKSZ_2];
+	__shared__ half d_d[BLKSZ_2][BLKSZ_2], d_c[BLKSZ_2][BLKSZ_2];
 	int base = k * BLKSZ_2;
 	int i = threadIdx.y;
 	int j = threadIdx.x;
@@ -68,19 +74,19 @@ __global__ void floyd_kernel_2(int k, int *devMat, int N)
 	int current_base = d_i * N + d_j;
 	d_c[i][j] = devMat[current_base]; // updating matrix
 	__syncthreads();
-	
-	int newD;
+
+	half newD;
 
 	if (blockIdx.y == 0) {
 		for (int t = 0; t < BLKSZ_2; t++) {
-			newD = d_d[i][t] + d_c[t][j];
-			if (newD < d_c[i][j])
+			newD = __hadd(d_d[i][t], d_c[t][j]);
+			if (__hlt(newD, d_c[i][j]))
 				d_c[i][j] = newD;
 		}
 	} else {
 		for (int t = 0; t < BLKSZ_2; t++) {
-			newD = d_c[i][t] + d_d[t][j];
-			if (newD < d_c[i][j])
+			newD = __hadd(d_c[i][t], d_d[t][j]);
+			if (__hlt(newD, d_c[i][j]))
 				d_c[i][j] = newD;
 		}
 	}
@@ -89,35 +95,35 @@ __global__ void floyd_kernel_2(int k, int *devMat, int N)
 }
 
 
-__global__ void floyd_kernel_3(int k, int *devMat, int N)
+__global__ void floyd_kernel_3(int k, half *devMat, int N)
 {
 	if (blockIdx.x == k || blockIdx.y == k) return;
-	
-	__shared__ int d_c[BLKSZ_3][BLKSZ_3], d_r[BLKSZ_3][BLKSZ_3];
+
+	__shared__ half d_c[BLKSZ_3][BLKSZ_3], d_r[BLKSZ_3][BLKSZ_3];
 	int base = k * BLKSZ_3;
 	int d_i = blockDim.y * blockIdx.y + threadIdx.y;
 	int d_j = blockDim.x * blockIdx.x + threadIdx.x;
 	int i = threadIdx.y;
 	int j = threadIdx.x;
 	int col_base = (base + i) * N + d_j;
-	int row_base = d_i * N + base + j; 
+	int row_base = d_i * N + base + j;
 	base = d_i * N + d_j;
-	
+
 	d_r[i][j] = devMat[col_base];
 	d_c[i][j] = devMat[row_base];
-	int oldD = devMat[base];
+	half oldD = devMat[base];
 	__syncthreads();
-	int newD;
+	half newD;
 	for (int t = 0; t < BLKSZ_3; t++) {
-		newD = d_c[i][t] + d_r[t][j];
-		if (newD < oldD)
+		newD = __hadd(d_c[i][t], d_r[t][j]);
+		if (__hlt(newD, oldD))
 			oldD = newD;
 	}
 	devMat[base] = oldD;
 }
 
 
-int floyd(int* devMat, int N)
+int floyd(half* devMat, int N)
 {
 	int N_blk = N / BLKSZ_1;
 
@@ -145,8 +151,9 @@ int main(int argc, char* argv[])
 {
 	int N;
 	int flag = 0;
-	int *mat, *ref, *result;
-	struct timespec start, end;	
+	int *mat, *ref;
+	half *result;
+	struct timespec start, end;
 	double diff1, diff2;
 
 	if (argc == 1) {
@@ -176,7 +183,7 @@ int main(int argc, char* argv[])
 	mat = (int *)malloc(N * N * sizeof(int));
 	ref = (int *)malloc(sizeof(int) * N * N);
 	GenMatrix(mat, N);
-	
+
 	if (flag) {
 		memcpy(ref, mat, sizeof(int) * N * N);
 		clock_gettime(CLOCK_MONOTONIC, &start);
@@ -185,8 +192,14 @@ int main(int argc, char* argv[])
 		diff1 = 1000000 * (end.tv_sec-start.tv_sec) + (end.tv_nsec-start.tv_nsec)/1000;
 	}
 
-	cudaMallocManaged(&result, N * N * sizeof(int));
-	cudaMemcpy(result, mat, sizeof(int) * N * N, cudaMemcpyHostToDevice);
+	cudaMallocManaged(&result, N * N * sizeof(half));
+//	cudaMemcpy(result, mat, sizeof(int) * N * N, cudaMemcpyHostToDevice);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			result[i * N + j] = approx_float_to_half((float)(mat[i * N + j]));
+		}
+	}
+
 	diff2 = 0;
 	floyd(result, N);
 	floyd(result, N);
@@ -199,11 +212,16 @@ int main(int argc, char* argv[])
 		printf("%d Problem SIZE ------ CUDA_APSP elasped time =%.2lfus\n", N, diff2);
 	}
 	if (flag) {
-		if (CmpArray(result, ref, N * N))
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				mat[i* N + j] = (int)half_to_float(result[i* N + j]);
+			}
+		}
+		if (CmpArray(mat, ref, N * N))
 			printf("Your result is correct.\n");
 		else
 			printf("Your result is wrong.\n");
-		
+
 		printf("ST_APSP elasped time   = %.2lf us\n", diff1);
 		printf("============================================\n");
 		printf("Speedup = %.2lf\n", diff1 / diff2);
